83776e3902c9359a5712aaf0963b807b64bc281b
jamesodoyle <james@stardev.co.za>
2023-02-14 19:26:58 +0200

    start

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import  React from 'react'
+
+const Header=()=>{
+return (
+    <div
+    style={{
+        
+        position:"absolute",
+        width:"100vw",
+    height:"30px",
+    backgroundColor:"lightBrown"}}>
+        <p>this is a header</p>
+    </div>
+)
+}
+
+export default Header
